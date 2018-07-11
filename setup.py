10ff924cc7c229c51110ca43c88398c8c2a5e01e
podhmo <ababjam61+github@gmail.com>
2018-07-11 21:20:36 +0900

    bump-version

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 try:
@@ -14,47 +13,39 @@ try:
 except IOError:
     README = CHANGES = ''
 
+install_requires = ['pip', 'distlib']
 
-install_requires = [
-    'pip',
-    'distlib'
-]
+docs_extras = []
 
+tests_require = ["evilunit"]
 
-docs_extras = [
-]
+testing_extras = tests_require + []
 
-tests_require = [
-    "evilunit"
-]
-
-testing_extras = tests_require + [
-]
-
-setup(name='ppic',
-      version='0.2.5',
-      description='ppic is python package information collector.',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: Implementation :: CPython",
-      ],
-      keywords='',
-      author="podhmo",
-      author_email="",
-      url="https://github.com/podhmo/ppic",
-      packages=find_packages(exclude=["tests"]),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      extras_require={
-          'testing': testing_extras,
-          'docs': docs_extras,
-      },
-      tests_require=tests_require,
-      test_suite="ppic.tests",
-      entry_points="""
+setup(
+    name='ppic',
+    version='0.2.6',
+    description='ppic is python package information collector.',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='',
+    author="podhmo",
+    author_email="",
+    url="https://github.com/podhmo/ppic",
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="ppic.tests",
+    entry_points="""
       [console_scripts]
       ppic = ppic:main
-""")
-
+"""
+)
